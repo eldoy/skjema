539f56674ed3e2fe39b98e0c54a811dece2bff00
Vidar Eldøy <vidar@eldoy.com>
2023-06-27 00:00:07 +0200

    Use orb

--- spec/tests/fields-test.js
+++ spec/tests/fields-test.js
@@ -1,29 +1,29 @@
 it('should have a checkbox field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.checkbox(models.article)
+  var result = skjema.fields.checkbox({}, models.article)
   t.ok(typeof result == 'string')
 })
 
 it('should have a file field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.file(models.article)
+  var result = skjema.fields.file({}, models.article)
   t.ok(typeof result == 'string')
 })
 
 it('should have a radio field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.radio(models.article)
+  var result = skjema.fields.radio({}, models.article)
   t.ok(typeof result == 'string')
 })
 
 it('should have a select field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.select(models.article)
+  var result = skjema.fields.select({}, models.article)
   t.ok(typeof result == 'string')
 })
 
 it('should have a string field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.string(models.article)
+  var result = skjema.fields.string({}, models.article)
   t.ok(typeof result == 'string')
 })
 
 it('should have a text field', async function ({ t, skjema, models }) {
-  var result = skjema.fields.text(models.article)
+  var result = skjema.fields.text({}, models.article)
   t.ok(typeof result == 'string')
 })
