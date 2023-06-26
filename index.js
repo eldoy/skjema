6347316bc832edeba22f9212d3224176e3baaaa0
Vidar Eldøy <vidar@eldoy.com>
2023-06-26 18:40:09 +0200

    Tedious require

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var loader = require('conficurse')
-
-module.exports = loader.load('./lib')
-
-// module.exports = {
-//   model: require('./lib/model.js'),
-//   form: require('./lib/form.js'),
-//   fields: {
-//     checkbox: require('./lib/fields/checkbox.js'),
-//     file: require('./lib/fields/file.js'),
-//     radio: require('./lib/fields/radio.js'),
-//     select: require('./lib/fields/select.js'),
-//     string: require('./lib/fields/string.js'),
-//     text: require('./lib/fields/text.js')
-//   },
-//   scripts: {
-//     handleDelete: require('./lib/scripts/handleDelete.js'),
-//     handleSave: require('./lib/scripts/handleSave.js'),
-//     handleUpload: require('./lib/scripts/handleUpload.js'),
-//     renderUploadImage: require('./lib/scripts/renderUploadImage.js')
-//   }
-// }
+module.exports = {
+  model: require('./lib/model.js'),
+  form: require('./lib/form.js'),
+  actions: {
+    count: require('./lib/actions/count.js'),
+    create: require('./lib/actions/create.js'),
+    delete: require('./lib/actions/delete.js'),
+    find: require('./lib/actions/find.js'),
+    get: require('./lib/actions/get.js'),
+    search: require('./lib/actions/search.js'),
+    update: require('./lib/actions/update.js'),
+    upload: require('./lib/actions/upload.js')
+  },
+  fields: {
+    checkbox: require('./lib/fields/checkbox.js'),
+    file: require('./lib/fields/file.js'),
+    radio: require('./lib/fields/radio.js'),
+    select: require('./lib/fields/select.js'),
+    string: require('./lib/fields/string.js'),
+    text: require('./lib/fields/text.js')
+  },
+  scripts: {
+    handleDelete: require('./lib/scripts/handleDelete.js'),
+    handleSave: require('./lib/scripts/handleSave.js'),
+    handleUpload: require('./lib/scripts/handleUpload.js'),
+    renderUploadImage: require('./lib/scripts/renderUploadImage.js')
+  }
+}
