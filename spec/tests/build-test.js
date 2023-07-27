1a4a9051b716ee8d6379f96655b99a84b5c6cae9
Vidar Eldøy <vidar@eldoy.com>
2023-07-27 16:34:14 +0200

    Pass test

--- spec/tests/build-test.js
+++ spec/tests/build-test.js
@@ -1,4 +1,4 @@
-it('should build a form', async function ({ t, skjema, models }) {
-  let result = await skjema.build({}, models.article)
+it('should build a form', async function ({ t, skjema, models, $ }) {
+  let result = await skjema.build($, models.article)
   t.ok(result.trim().startsWith('<form'))
 })
